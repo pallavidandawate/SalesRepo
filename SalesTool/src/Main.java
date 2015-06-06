8dc4a125778c8dde7fe5d3d0eb0f8f9deacfd9ff
pallavi <pallavi.dandawate@gmail.com>
2015-06-06 13:20:28 +0530

    initial checkin sales tool

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 SalesData data = new SalesData();
+	 displayGreeting();
+	 data.display();
+	 }
+	
+	
+	public static void displayGreeting()
+	{
+		System.out.println("hello world 1");
+		System.out.println("hello world 2");
+			}
+	
+
+}
---
+++ SalesTool/src/SalesData.java
@@ -0,0 +1,8 @@
+
+public class SalesData {
+	int data[]={0,4,42};
+	
+	public void display(){
+		System.out.println("to be implemented...");
+	}
+}
